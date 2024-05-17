2643482a79d3576adaa2019cdaa0ae0dfb676d47
aline <linegcruz@gmail.com>
2024-05-17 09:18:48 -0300

    fix(renderflex_overflowed): added SingleChildScrollView wrapping Column to implement the screen.

--- lib/average_calculator/average_form.dart
+++ lib/average_calculator/average_form.dart
@@ -20,56 +20,58 @@ class _AverageFormState extends State<AverageForm> {
         padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Nome'),
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'eMail'),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Nota 1'),
-                      keyboardType: TextInputType.number,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Nome'),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'eMail'),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Nota 1'),
+                        keyboardType: TextInputType.number,
+                      ),
                     ),
-                  ),
-                  SizedBox(width: 8.0),
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Nota 2'),
-                      keyboardType: TextInputType.number,
+                    SizedBox(width: 8.0),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Nota 2'),
+                        keyboardType: TextInputType.number,
+                      ),
                     ),
-                  ),
-                  SizedBox(width: 8.0),
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Nota 3'),
-                      keyboardType: TextInputType.number,
+                    SizedBox(width: 8.0),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Nota 3'),
+                        keyboardType: TextInputType.number,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('Calcula Média'),
-              ),
-              SizedBox(height: 16.0),
-              Text('Resultado:'),
-              Text('Nome: '),
-              Text('eMail: '),
-              Text('Notas: '),
-              Text('Média: '),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('Apaga os Campos'),
-              ),
-            ],
+                  ],
+                ),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Calcula Média'),
+                ),
+                SizedBox(height: 16.0),
+                Text('Resultado:'),
+                Text('Nome: '),
+                Text('eMail: '),
+                Text('Notas: '),
+                Text('Média: '),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Apaga os Campos'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
